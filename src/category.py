e2f58ab86fcf9ce4372d71402a82f36a0ac9f965
Klimchuk_Sergei <klim.work@mail.ru>
2025-05-13 21:26:15 +0300

    дописаны функциональности для product и category

--- src/category.py
+++ src/category.py
@@ -5,6 +5,24 @@ class Category:
     def __init__(self, name: str, description: str, products: list):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = []  # приватый список
         Category.category_count += 1
-        Category.product_count += len(products)
+
+        for product in products:
+            self.add_product(product)
+
+    def add_product(self, product):
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self):
+        return self.__products.copy()
+
+    @property
+    def products(self):
+        result = []
+        for product in self.__products:
+            line = f"{product.name}, {int(product.price)} руб. Остаток: {product.quantity} шт."
+            result.append(line)
+        return "\n".join(result)
